488cd037d8ed78c0c23bf638348e72685d1107f4
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-04-05 15:22:49 -0400

    Lets try again

--- app/game.py
+++ app/game.py
@@ -8,20 +8,20 @@ choices = ['rock', 'paper', 'scissors']
 def determine_winner(p1, p2):
     return "rock"
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
 
     u = input(f"Please choose one of {choices}: ").lower()
     print("USER CHOICE:", u)
     if u not in choices:
         print("OOPS, TRY AGAIN")
         exit()
-    
-    
+
+
     # COMPUTER SELECTION
     c = choice(choices)
     print("COMPUTER CHOICE:", c)
-    
-    
+
+
     # DETERMINATION OF WINNER
     if u == c:
         print("It's a tie!")
