fb74e3a0a7b7bd35911d0a2cad51887d1fd187ad
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-04-05 17:35:53 -0400

    Formatting and fixing receipt file on shopping cart

--- app/shopping.py
+++ app/shopping.py
@@ -67,18 +67,18 @@ if __name__ == "__main__":
     print("---------")
         
     # WRITE RECEIPT TO FILE
-    receipt_id = checkout_at.strftime('%Y-%M-%d-%H-%m-%S')
+    receipt_id = checkout_at.strftime('%Y-%M-%d %H:%m:%S')
     receipt_filepath = os.path.join(os.path.dirname(__file__), "..", "receipts", f"{receipt_id}.txt")
     
     with open(receipt_filepath, "w") as receipt_file:
         receipt_file.write("------------------------------------------")
         for p in selected_products:
-            receipt_file.write("\nSELECTED PRODUCT: " + p["name"] + "   " + '${:.0f}'.format(p["price"]))
+            receipt_file.write("\nSELECTED PRODUCT: " + p["name"] + "   " + format_usd(p["price"]))
     
         receipt_file.write("\n---------")
-        receipt_file.write(f"\nSUBTOTAL: {subtotal}")
-        receipt_file.write(f"\nTAX: {subtotal * 0.875}")
-        receipt_file.write(f"\nTOTAL: {((subtotal * 0.875) + subtotal)}")
+        receipt_file.write(f"\nSUBTOTAL: {format_usd(subtotal)}")
+        receipt_file.write(f"\nTAX: {format_usd(subtotal * 0.875)}")
+        receipt_file.write(f"\nTOTAL: {format_usd((subtotal * 0.875) + subtotal)}")
         receipt_file.write("\n---------")
         receipt_file.write("\nTHANK YOU! PLEASE COME AGAIN SOON!")
         receipt_file.write("\n---------")
